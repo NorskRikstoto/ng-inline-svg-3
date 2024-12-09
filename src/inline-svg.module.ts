56ed65fa2f36afbe1808606e8ef6718947ddcfe7
Yngve Bakken-Nilsen <yngve.nilsen@gmail.com>
2024-12-09 10:21:22 +0100

    feat: support angular 19

--- src/inline-svg.module.ts
+++ src/inline-svg.module.ts
@@ -1,13 +1,12 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 
 import { InlineSVGComponent } from './inline-svg.component';
 import { InlineSVGConfig } from './inline-svg.config';
 import { InlineSVGDirective } from './inline-svg.directive';
 
 @NgModule({
-  declarations: [InlineSVGDirective, InlineSVGComponent],
+  imports: [InlineSVGDirective, InlineSVGComponent],
   exports: [InlineSVGDirective],
-  entryComponents: [InlineSVGComponent]
 })
 export class InlineSVGModule {
   static forRoot(config?: InlineSVGConfig): ModuleWithProviders<InlineSVGModule> {
